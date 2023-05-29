3639dbeac6c7b5a8218c1002c62827b4b731fd47
Diego <diego.valdez.r@tecsup.edu.pe>
2023-05-29 17:33:23 -0500

    first commit

---
+++ Ejercicio09_Valdez/Ejercicio09_Valdez/App.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Ejercicio09_Valdez
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+            //MainPage = new ExplicitDemo();
+            //MainPage = new ExplicitControlDemo();
+            //MainPage = new ImplicitDemo();
+            //MainPage = new ImplicitControlDemo();
+            //MainPage = new GlobalDemo();
+            //MainPage = new InheritanceDemo();
+            //MainPage = new DynamicDemo();
+            MainPage = new CascadingStyleSheets();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
